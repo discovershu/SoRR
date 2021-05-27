73a87898dc6f7e08e7175aecf5bc2146e0daae86
Shu Hu <shu.hu@foxmail.com>
2021-05-27 17:35:38 -0400

    Update AoRR_Run_MNIST_noise.py

--- AoRR_multiclass/AoRR_Run_MNIST_noise.py
+++ AoRR_multiclass/AoRR_Run_MNIST_noise.py
@@ -11,17 +11,9 @@ import itertools
 # from keras.utils import np_utils
 from scipy.io import loadmat, savemat
 from sklearn.preprocessing import OneHotEncoder
-from logisticregression_Avg import LogisticRegression_Avg
-from logisticregression_Avg_train_val import LogisticRegression_Avg_train_val
-from logisticregression_ATK import LogisticRegression_ATK
-from logisticregression_ATK_train_val import LogisticRegression_ATK_train_val
-from logisticregression_Top1 import LogisticRegression_Top1
-from logisticregression_Top1_train_val import LogisticRegression_Top1_train_val
-from logisticregression_AoRR_GD import LogisticRegression_AoRR_GD
-from logisticregression_AoRR_GD_train_val import LogisticRegression_AoRR_GD_train_val
-from logisticregression_AoRR_GD_train import LogisticRegression_AoRR_GD_train
-from logisticregression_AoRR_DC import LogisticRegression_AoRR_DC
-from logisticregression_AoRR_DC_train_val import LogisticRegression_AoRR_DC_train_val
+from AoRR_multiclass.logisticregression_Avg_train_val import LogisticRegression_Avg_train_val
+from AoRR_multiclass.logisticregression_ATK_train_val import LogisticRegression_ATK_train_val
+from AoRR_multiclass.logisticregression_AoRR_DC_train_val import LogisticRegression_AoRR_DC_train_val
 
 if __name__ == '__main__':
 
@@ -85,25 +77,7 @@ if __name__ == '__main__':
     # print('Index: {}; Label: {}'.format(example_idx, y_train[example_idx]))
 
     #### Select classifier and set parameters
-    if Method == "Avg":
-        classifier = LogisticRegression_Avg(lr=0.1, reg=1e-5, num_iter=1000, seed=1234,
-                                            ratio=noise_ratio)  #### average case
-    elif Method == "ATK":
-        classifier = LogisticRegression_ATK(lr=0.1, reg=1e-5, num_iter=5000, k_value=kvalue, seed=1234)  #### ATK case
-    elif Method == "Top1":
-        classifier = LogisticRegression_Top1(lr=0.01, reg=1e-5, num_iter=10000, seed=1234)  #### Top1 case
-    elif Method == "AoRR_GD":
-        classifier = LogisticRegression_AoRR_GD(lr=0.03, reg=1e-5, num_iter=5000, k_value=kvalue, m_value=mvalue,
-                                                seed=1234)
-    elif Method == "AoRR_DC":
-        classifier = LogisticRegression_AoRR_DC(lr=0.2, reg=1e-5, outer_iter=100, inner_iter=1000, k_value=kvalue,
-                                                m_value=mvalue, \
-                                                dataname=data_name, Modelname=Model_name, seed=1234)
-    elif Method == "AoRR_GD_train_val":
-        classifier = LogisticRegression_AoRR_GD_train_val(lr_train=0.4, lr_val=0.5, reg=1e-5, num_iter=20000,
-                                                          seed=1234, \
-                                                          milestone=50, n_epochs_stop=50)
-    elif Method == "Avg_GD_train_val":
+    if Method == "Avg_GD_train_val":
         classifier = LogisticRegression_Avg_train_val(lr=0.1, reg=1e-5, num_iter=10000, seed=1234)
     elif Method == "AoRR_DC_train_val":
         classifier = LogisticRegression_AoRR_DC_train_val(lr_train=0.4, lr_val=0.5, train_iter=20000, reg=1e-5,
@@ -124,5 +98,3 @@ if __name__ == '__main__':
     print('Top-{} Accuracy (test) :'.format(5), classifier.predict_topk_acc(y_test, X_test, kk_value=5, X_modify = True))
     print("total_time: ", t_total)
     # np.save('parameters/MNIST_noise/'+ Model_name+'_'+Method+'_'+'noise_ratio'+'_'+str(noise_ratio)+ '.npy', parameters)
-
-    print("shu")
